75a7c5d1794cb4119fd7da5207c2e5cacf5b2aa3
Luis <luisriveros26@gmail.com>
2022-06-04 18:04:22 -0600

    more code to path.js

--- utils/path.js
+++ utils/path.js
@@ -7,6 +7,7 @@ const viewAllEmployees = () => {
     INNER JOIN roles ON (roles.id = employee.role_id)
     INNER JOIN department ON (department.id = roles.department_id)
     ORDER BY employee.id;`
+
     db.query(sql, (err, res) => {
         if (err) {
             console.log(err);
@@ -14,4 +15,49 @@ const viewAllEmployees = () => {
         }
         console.table(res);
     })
-}
+};
+
+const viewAllDepartments = () => {
+    const sql = `SELECT department.id, department.name AS Department
+    FROM department;`
+
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+    })
+};
+
+const viewAllRoles = () => {
+    const sql = `SELECT roles.id, roles.title AS role, roles.salary, department.name AS department
+    FROM roles
+    INNER JOIN department ON (department.id = roles.department_id);`;
+
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+    })
+};
+
+const viewByDepartment = (department) => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN roles ON (roles.id = employee.role_id)
+    INNER JOIN department ON (department.id = roles.department_id)
+    WHERE department.name = '${department}'
+    ORDER BY employee.id;`
+
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(res);
+    });
+};
